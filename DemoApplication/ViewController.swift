b2b5413cebbd57e26178e30ac4d447ce3f41690d
Sharvari Chavan <koptotech009@gmail.com>
2023-08-23 17:00:44 +0530

    One label is created

--- DemoApplication/ViewController.swift
+++ DemoApplication/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lblName.text = "Hello"
     }
 
 
